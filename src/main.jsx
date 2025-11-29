cf9a6a62a9df7ed854f6e45df9efe0689caa19c6
Milan <milanmekanna@gmail.com>
2025-11-29 15:13:41 +0530

    Add SPA routing fix for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import TargetCursor from './Components/TargetCursor.jsx'
+// import TargetCursor from './Components/TargetCursor.jsx'
 
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
-    <TargetCursor
+    {/* <TargetCursor
       spinDuration={2}
       hideDefaultCursor={true}
       parallaxOn={true}
       cursorColor="#561C24"
       
-    />
+    /> */}
     <App />
   </BrowserRouter>
 
